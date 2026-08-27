d3359ebcd548579f8fe45c14e79e2843a3cb2186
Shadzzzzzz <workahad.16@gmail.com>
2026-08-27 12:11:51 +0530

    Add initial Java program

---
+++ DAY1/P1.java
@@ -0,0 +1,16 @@
+// variables in java
+public class P1{
+    public static void main(String[] args){
+        short shortnumber = 32000;
+        int age = 25;
+
+        float tempetarure = 36.6f;
+        double price = 99.99;
+        char grade = 'A';
+        boolean isEnrolled = true;
+
+        String student = "Azeem";
+        System.out.println("Student = " + student);
+
+    }
+}
